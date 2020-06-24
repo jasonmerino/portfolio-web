30b1e1630ae2caac213974f16d5bb0f2fb53c224
Jason Merino <jason.merino@ferguson.com>
2020-06-24 12:02:25 -0700

    Add custom Next.js app file.

---
+++ pages/_app.tsx
@@ -0,0 +1,69 @@
+import "tachyons";
+import { FC } from "react";
+import { AppProps } from "next/app";
+import { Header } from "../components/header";
+
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Header />
+      <section className={`w-100 mw8 center bg-white`}>
+        <Component {...pageProps} />
+      </section>
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        body {
+          overflow-x: hidden;
+        }
+
+        h1 {
+          line-height: 2.5rem;
+        }
+
+        a {
+          color: #356d5f;
+        }
+
+        #__next {
+          min-height: 100%;
+          overflow-x: hidden;
+        }
+
+        /* colors */
+        .bg-dark1 {
+          background-color: #383838;
+        }
+
+        .dark1 {
+          color: #383838;
+        }
+
+        .bg-darkTransparent1 {
+          background: rgba(0, 0, 0, 0.3);
+        }
+
+        .light1 {
+          color: #fff;
+        }
+
+        .color-lets-garden {
+          color: #60c1a9;
+        }
+
+        .shadow-1-hover:hover {
+          box-shadow: 0 0 20px #e8e8e8;
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default App;
