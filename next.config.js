b8c95b84f260344d79beb7c5bad4737e6ac7fbea
Jason Merino <jasonmerino@gmail.com>
2021-09-18 07:39:08 -0700

    Add referral link for grove. (#16)

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ module.exports = {
         destination: 'https://my.aspiration.com/app/token/referral/1Q2L0EMVOQC4M927',
         permanent: true,
       },
+      {
+        source: '/grove',
+        destination: 'https://www.grove.co/referrer/214401780/',
+        permanent: true,
+      },
     ]
   },
 }
